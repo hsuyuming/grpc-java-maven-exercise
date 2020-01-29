7add3ed8b428f27123312e3d5196c97ec91f3ef1
hsuyuming <abego452@gmail.com>
2020-01-30 01:45:57 +0800

    Implement SSL certification

--- src/main/java/com/github/simpleabehsu/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/github/simpleabehsu/grpc/greeting/client/GreetingClient.java
@@ -2,8 +2,12 @@ package com.github.simpleabehsu.grpc.greeting.client;
 
 import com.proto.greet.*;
 import io.grpc.*;
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
 import io.grpc.stub.StreamObserver;
 
+import javax.net.ssl.SSLException;
+import java.io.File;
 import java.util.Arrays;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -11,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 public class GreetingClient {
     
 
-    public void run(){
+    public void run() throws SSLException {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",50051)
                 .usePlaintext()
                 .build();
@@ -20,11 +24,24 @@ public class GreetingClient {
 //        doServerStreamingCall(channel);
 //        doClientStreamingCall(channel);
 //        doBiDiStreamingCall(channel);
-        doUnaryCallWithDeadline(channel);
+//        doUnaryCallWithDeadline(channel);
 
         //do something
         System.out.println("Shutting down channel");
         channel.shutdown();
+
+
+        ManagedChannel securityChannel = NettyChannelBuilder.forAddress("localhost",50051)
+                .sslContext(
+                        GrpcSslContexts.forClient().trustManager(
+                            new File("ssl/ca.crt")
+                        ).build()
+                )
+                .build();
+
+        doUnaryCall(securityChannel);
+        System.out.println("Shutting down securityChannel");
+        securityChannel.shutdown();
 
     }
 
@@ -255,7 +272,7 @@ public class GreetingClient {
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SSLException {
         System.out.println("Hello I'm a gRPC client");
 
         GreetingClient main = new GreetingClient();
--- src/main/java/com/github/simpleabehsu/grpc/greeting/server/GreetingServer.java
+++ src/main/java/com/github/simpleabehsu/grpc/greeting/server/GreetingServer.java
@@ -3,15 +3,24 @@ package com.github.simpleabehsu.grpc.greeting.server;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
+import java.io.File;
 import java.io.IOException;
 
 public class GreetingServer {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("Hello gRPC");
+//
+//        Server server = ServerBuilder.forPort(50051)
+//                .addService(new GreetServiceImpl())
+//                .build();
 
         Server server = ServerBuilder.forPort(50051)
                 .addService(new GreetServiceImpl())
+                .useTransportSecurity(
+                        new File("ssl/server.crt"),
+                        new File("ssl/server.pem")
+                )
                 .build();
 
         server.start();
