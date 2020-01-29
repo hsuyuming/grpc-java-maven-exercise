ebfa38b3bb6f1a3cf0567715bafc0307dc299353
hsuyuming <abego452@gmail.com>
2020-01-29 13:12:30 +0800

    Finish server streaming api example

--- src/main/java/com/github/simpleabehsu/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/github/simpleabehsu/grpc/greeting/client/GreetingClient.java
@@ -1,7 +1,6 @@
 package com.github.simpleabehsu.grpc.greeting.client;
 
-import com.proto.greet.GreetRequest;
-import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetManyTimesRequest;
 import com.proto.greet.GreetServiceGrpc;
 import com.proto.greet.Greeting;
 import io.grpc.ManagedChannel;
@@ -24,21 +23,35 @@ public class GreetingClient {
         // Created a greet service client (blocking - synchronous)
         GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
 
-        // Created a protocol buffer greeting message
-        Greeting greeting = Greeting.newBuilder()
-                .setFirstName("abe")
-                .setLastName("Hsu")
+//        Unary Example
+//        // Created a protocol buffer greeting message
+//        Greeting greeting = Greeting.newBuilder()
+//                .setFirstName("abe")
+//                .setLastName("Hsu")
+//                .build();
+//
+//        // Created a protocol buffer request message
+//        GreetRequest greetRequest = GreetRequest.newBuilder()
+//                .setGreeting(greeting)
+//                .build();
+//
+//        // call the RPC and get back a GreetResponse (protocol buffers)
+//        GreetResponse greetResponse =  greetClient.greet(greetRequest);
+//
+//        System.out.println(greetResponse.getResult());
+
+
+        // Server Streaming
+        //we prepare the request
+        GreetManyTimesRequest greetManyTimesRequest = GreetManyTimesRequest.newBuilder()
+                .setGreeting(Greeting.newBuilder().setFirstName("abehsu").build())
                 .build();
 
-        // Created a protocol buffer request message
-        GreetRequest greetRequest = GreetRequest.newBuilder()
-                .setGreeting(greeting)
-                .build();
-
-        // call the RPC and get back a GreetResponse (protocol buffers)
-        GreetResponse greetResponse =  greetClient.greet(greetRequest);
-
-        System.out.println(greetResponse.getResult());
+        // we stream the reponses ( in a blocking manner)
+        greetClient.greetManyTimes(greetManyTimesRequest)
+                .forEachRemaining( greetManyTimesResponse -> {
+                    System.out.println(greetManyTimesResponse.getResult());
+                });
 
 
 
